4b6412e7b1483555afddfd51174296b4ce20f85f
Brendan Wong <mrbrendanwong@gmail.com>
2017-07-17 17:02:38 -0700

    cleaned up

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
+#include "mainwindow.h"
 #include <QApplication>
-#include <QLabel>
-#include <QSpinBox>
-#include <QLineEdit>
-#include <QComboBox>
-#include <QPushButton>
-#include <QDateEdit>
-#include <QFormLayout>
+
+void mainWindow(QTabWidget *tabWidget);
+void customTab(QWidget *customWindow, QTabWidget *tabWidget);
+void templateTab(QWidget *templateTab, QTabWidget *tabWidget);
 
 int main(int argc, char *argv[])
 {
@@ -13,75 +11,17 @@ int main(int argc, char *argv[])
 
     QTabWidget *tabWidget = new QTabWidget;
 
+    mainWindow(tabWidget);
+
     QWidget *customWindow = new QWidget;
     QWidget *templateWindow = new QWidget;
 
-    //Window properties
-    customWindow->setWindowTitle("G-Code Generator");
-    templateWindow->setWindowTitle("Custom G-Code Generator");
-    tabWidget->setMinimumSize(500, 300);
+    customTab(customWindow, tabWidget);
+    templateTab(templateWindow, tabWidget);
 
 
-    //Label creation
-    QLabel *customLabel = new QLabel;
-    QLabel *templateLabel = new QLabel;
-
-    //Label text
-    customLabel->setText("Custom Print");
-    templateLabel->setText("Template tab");
-
-    //Field creation
-    QLineEdit *nameEdit = new QLineEdit;
-    QDateEdit *dateEdit = new QDateEdit;
-    QSpinBox *heightEdit = new QSpinBox;
-    QSpinBox *widthEdit = new QSpinBox;
-    QSpinBox *positionEdit = new QSpinBox;
-    QComboBox *materialEdit = new QComboBox;
-
-    QPushButton *nextCustom = new QPushButton;
-
-    //Constraints/properties
-    templateLabel->setAlignment(Qt::Alignment(5));
-
-    customLabel->setAlignment(Qt::Alignment(5));
-    heightEdit->setRange(1, 7);
-    widthEdit->setRange(1, 7);
-    positionEdit->setRange(1, 4);
-    materialEdit->addItem("Calcium Carbonate");
-    materialEdit->addItem("HPR");
-    materialEdit->addItem("ABTS");
-    dateEdit->setDate(QDate::currentDate());
-    nextCustom->setMaximumSize(50, 20);
-    nextCustom->setText("Next");
-
-    QFormLayout *formLayout = new QFormLayout;
-    QHBoxLayout *horizontalLayout = new QHBoxLayout;
-
-    formLayout->addWidget(customLabel);
-
-    formLayout->addRow("&Name:", nameEdit);
-    formLayout->addRow("&Date:", dateEdit);
-    formLayout->addRow("&Height:", heightEdit);
-    formLayout->addRow("&Width:", widthEdit);
-    formLayout->addRow("&Position:", positionEdit);
-    formLayout->addRow("&Material:", materialEdit);
-    formLayout->addWidget(nextCustom);
-
-    horizontalLayout->addWidget(templateLabel);
-
-    templateWindow->setLayout(horizontalLayout);
-    customWindow->setLayout(formLayout);
-
-    tabWidget->addTab(templateWindow, "Template");
-    tabWidget->addTab(customWindow, "Custom");
-
-    //need to add in functionality for closing
-    //and frameless dragging  before removal
-    //tabWidget->setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
 
     tabWidget->show();
 
     return prog.exec();
 }
-
-
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QApplication>
+#include <QLabel>
+#include <QSpinBox>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QPushButton>
+#include <QDateEdit>
+#include <QFormLayout>
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent),
                                           ui(new Ui::MainWindow)
@@ -10,4 +18,73 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent),
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void mainWindow(QTabWidget *tabWidget)
+{
+    tabWidget->setMinimumSize(500, 300);
+
+}
+
+void customTab(QWidget *customWindow, QTabWidget *tabWidget)
+{
+    QLabel *customLabel = new QLabel;
+    customLabel->setText("Custom Print");
+
+    QLineEdit *nameEdit = new QLineEdit;
+    QDateEdit *dateEdit = new QDateEdit;
+    QSpinBox *heightEdit = new QSpinBox;
+    QSpinBox *widthEdit = new QSpinBox;
+    QSpinBox *positionEdit = new QSpinBox;
+    QComboBox *materialEdit = new QComboBox;
+    QPushButton *nextCustom = new QPushButton;
+
+    customLabel->setAlignment(Qt::Alignment(5));
+    heightEdit->setRange(1, 7);
+    widthEdit->setRange(1, 7);
+    positionEdit->setRange(1, 4);
+    materialEdit->addItem("Calcium Carbonate");
+    materialEdit->addItem("HPR");
+    materialEdit->addItem("ABTS");
+    dateEdit->setDate(QDate::currentDate());
+    nextCustom->setMaximumSize(50, 20);
+    nextCustom->setText("Next");
+
+    QFormLayout *formLayout = new QFormLayout;
+
+    formLayout->addWidget(customLabel);
+
+    formLayout->addRow("&Name:", nameEdit);
+    formLayout->addRow("&Date:", dateEdit);
+    formLayout->addRow("&Height:", heightEdit);
+    formLayout->addRow("&Width:", widthEdit);
+    formLayout->addRow("&Position:", positionEdit);
+    formLayout->addRow("&Material:", materialEdit);
+    formLayout->addWidget(nextCustom);
+
+    customWindow->setLayout(formLayout);
+
+    tabWidget->addTab(customWindow, "Custom");
+
+
+}
+
+void templateTab(QWidget *templateWindow, QTabWidget *tabWidget)
+{
+    QLabel *templateLabel = new QLabel;
+    templateLabel->setText("Template tab");
+    QPushButton *quitButton = new QPushButton;
+    QObject::connect(quitButton, SIGNAL(clicked()), qApp, SLOT(quit()));
+
+    templateLabel->setAlignment(Qt::Alignment(5));
+
+    QHBoxLayout *templateLayout = new QHBoxLayout;
+
+    templateLayout->addWidget(templateLabel);
+    templateLayout->addWidget(quitButton);
+
+    templateWindow->setLayout(templateLayout);
+
+    tabWidget->addTab(templateWindow, "Template");
+
 }
