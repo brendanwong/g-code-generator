84ca49dc7430a5c2455fdb66a9da03e0c54f1f85
Brendan Wong <mrbrendanwong@gmail.com>
2017-08-24 15:15:12 -0700

    added IO functionality

--- pagetwo.cpp
+++ pagetwo.cpp
@@ -1,6 +1,7 @@
 #include "pagetwo.h"
 #include "wizard.h"
 #include <QFileDialog>
+#include <QTextStream>
 
 PageTwo::PageTwo(QWidget *parent) : QWidget(parent)
 {
@@ -18,24 +19,56 @@ PageTwo::PageTwo(QWidget *parent) : QWidget(parent)
 
 void PageTwo::updateOutput(const QString output)
 {
+    this->output = output;
     if (firstOutput == true)
     {
+        firstOutput = false;
+
         textEdit = new QTextEdit;
         textEdit->setReadOnly(true);
         textEdit->setMinimumSize(300, 250);
 
         textEdit->setPlaceholderText("Output code should go here");
-
         textEdit->setText(output);
+
         layout->addWidget(textEdit);
-        firstOutput = false;
+
         QPushButton *exportButton = new QPushButton(tr("Export as Text File"));
         layout->addWidget(exportButton);
         QObject::connect(exportButton, SIGNAL(clicked(bool)), this, SLOT(onExportButtonClicked()));
-
     } else
         onNewOutput(output);
     //otherwise, new output box will be created every single time
+}
+
+
+
+
+
+void PageTwo::onExportButtonClicked()
+{
+    QString saveDirectory;
+    saveDirectory = QFileDialog::getExistingDirectory(this, tr("Export G-Code File"), title);
+    //need to connect the save button to preFileIO()
+    fileOutput(saveDirectory);
+
+
+}
+
+
+
+
+
+void PageTwo::fileOutput(QString directory)
+{
+       title = directory + "/" +title;
+       title += ".txt";
+       QFile file(title);
+       file.open(QIODevice::WriteOnly | QIODevice::Text);
+       QTextStream out(&file);
+       out << this->output;
+       file.close();
+
 }
 
 
@@ -47,16 +80,6 @@ void PageTwo::onNewOutput(QString output)
 
     textEdit->setText(output);
     QApplication::processEvents();
-}
-
-
-
-
-
-void PageTwo::onExportButtonClicked()
-{
-    QString ayylmao = "text.txt";
-    ayylmao = QFileDialog::getSaveFileName(this, tr("Export G-Code File"), title);
 }
 
 
--- pagetwo.h
+++ pagetwo.h
@@ -18,12 +18,14 @@ private slots:
     void onNewOutput(QString output);
     void onExportButtonClicked();
     void updateTitle(QString title);
+    void fileOutput(QString directory);
 
 private:
     QFormLayout *layout;
     QTextEdit *textEdit;
     bool firstOutput;
     QString title;
+    QString output;
 };
 
 #endif // PAGETWO_H
